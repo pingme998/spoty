943c25f88a589c2fbeac287f3cc4880d99fc4d34
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-10-09 00:35:58 +0530

    Update e.sh

--- e.sh
+++ e.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo 'tre' >/command.txt 
-echo "rclone rcd --rc-serve --rc--addr=0.0.0.0:$PORT" >>/command.txt
+echo "rclone rcd --rc-serve --rc-addr=0.0.0.0:$PORT" >>/command.txt
 chmod +x /command.txt
 parallel < /command.txt
