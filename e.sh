c536cee90a5a9036fa705e5ea41372a833df811e
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-10-10 01:49:47 +0530

    Update e.sh

--- e.sh
+++ e.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-echo 'tre' >/command.txt 
-echo "rclone rcd --rc-serve --rc-addr=0.0.0.0:$PORT" >>/command.txt
-chmod +x /command.txt
-parallel < /command.txt
+sed "s|thesuperurl|$|g"
