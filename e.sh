6d3fa62b524952c8b41fd397589e7e9ec18e6c6a
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-10-10 08:52:31 +0530

    Update e.sh

--- e.sh
+++ e.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "$INVITEURL"
 sed -i "s|thesuperurl|$INVITEURL|g" /etc/supervisor/conf.d/supervisord.conf
-
+cat /etc/supervisor/conf.d/supervisord.conf
+echo $PORT >/PORT
 /usr/bin/supervisord
