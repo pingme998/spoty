9845d4b94cdbf19fe0564e14247695c9a7d1cfea
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-10-16 17:02:41 +0530

    Update e.sh

--- e.sh
+++ e.sh
@@ -5,6 +5,5 @@ mkdir /root/.config/rclone
 wget "$CONFIG_IN_URL" -O /.rclone.conf
 wget "$CONFIG_IN_URL" -O /root/.config/rclone/rclone.conf
 echo $PORT >/PORT
-cat /default |sed 's|therandomport|$(cat /PORT)|g' > /etc/nginx/sites-enabled/default
 chmod +x /etc/nginx/sites-enabled/default
 /usr/bin/supervisord
